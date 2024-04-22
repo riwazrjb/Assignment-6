4d6f7983f3ca69b1bbd9c842b61f963bf5d344cb
riwazrjb <158445899+riwazrjb@users.noreply.github.com>
2024-04-22 00:10:09 -0700

    added comments

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const root = document.getElementById('root');
-
+//creating an area to enter text 
 const text = document.createElement('textarea');
 text.placeholder= 'Write something ';
 root.appendChild(text);
-
+//creating a submit button
 const submitButton = document.createElement('button');
 submitButton.textContent='submit';
 submitButton.addEventListener('click',submissionProcess);
 root.appendChild(submitButton)
-
+// does the processs aftere the paragraph is submitted by the user
 function submissionProcess(){
     const words = text.value.trim();
     const tokenWords= words.split(/\s+/);
     
     const frequencyTable={};
+    // counts freqency of the word 
     tokenWords.forEach(word=> {
          frequencyTable[word]= frequencyTable[word]? frequencyTable[word] +1 :1;
     });
-
+// sorting based on the frequency of the words on the array frequencytable
     const sort = Object.keys(frequencyTable).sort((a,b)=> frequencyTable[b]-frequencyTable[a]);
 
     const wordHierarchy = sort.slice(0,5);
-
+// creating a table and adding the data to it based on the frequency of words used in the input 
      const table = document.createElement('table');
     const tableHead = document.createElement('thead');
     const tableBody = document.createElement('tbody');
@@ -47,6 +48,6 @@ function submissionProcess(){
     table.appendChild(tableBody);
     root.appendChild(table);
 
-    // Log frequency table
+    // Prints frequency table
     console.log('Frequency Table:', frequencyTable);
 }
